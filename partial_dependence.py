56eeaa33deaae196666e3b3590fa68241214e3b8
mandalsubhajit <mandalsubhajit@gmail.com>
2018-05-24 19:08:15 +0530

    Upload partial dependence code

--- partial_dependence.py
+++ partial_dependence.py
@@ -9,14 +9,13 @@ Created on Wed May 23 21:12:54 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_partial_dependence(clf, X, features, n_cols=3, figsize=(10, 10)):
-    X_temp = X.copy().values
-    
+def plot_partial_dependence(clf, X, features, n_cols=3, figsize=(10, 10)):    
     fig = plt.figure(figsize=figsize)
     nrows=int(np.ceil(len(features)/float(n_cols)))
     ncols=min(n_cols, len(features))
     axs = []
     for i, f_id in enumerate(features):
+        X_temp = X.copy().values
         ax = fig.add_subplot(nrows, ncols, i + 1)
         
         x_scan = np.linspace(np.percentile(X_temp[:, f_id], 0.1), np.percentile(X_temp[:, f_id], 99.5), 10)
@@ -36,6 +35,7 @@ def plot_partial_dependence(clf, X, features, n_cols=3, figsize=(10, 10)):
     axs.append(ax)
     fig.subplots_adjust(bottom=0.15, top=0.7, left=0.1, right=0.95, wspace=0.4,
                         hspace=0.3)
+    fig.tight_layout()
     return fig, axs
 
 #fig, axs = plot_partial_dependence(clf, X_train, range(10), figsize=(10, 15))
