c673a302f531c1a3eee93c61124012fd5a763520
mandalsubhajit <mandalsubhajit@gmail.com>
2018-05-24 08:04:09 +0530

    Upload partial dependence code

--- partial_dependence.py
+++ partial_dependence.py
@@ -9,11 +9,10 @@ Created on Wed May 23 21:12:54 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_partial_dependence(clf, X, features, n_cols=3):
+def plot_partial_dependence(clf, X, features, n_cols=3, figsize=(10, 10)):
     X_temp = X.copy().values
     
-    fig = plt.figure()
-    plt.tight_layout()
+    fig = plt.figure(figsize=figsize)
     nrows=int(np.ceil(len(features)/float(n_cols)))
     ncols=min(n_cols, len(features))
     axs = []
@@ -31,9 +30,12 @@ def plot_partial_dependence(clf, X, features, n_cols=3):
         
         # Plot partial dependence
         ax.plot(x_scan, y_partial, '-', color = 'green', linewidth = 1)
-        ax.set_xlim(min(x_scan), max(x_scan))
+        ax.set_xlim(min(x_scan)-0.1*(max(x_scan)-min(x_scan)), max(x_scan)+0.1*(max(x_scan)-min(x_scan)))
+        ax.set_ylim(min(y_partial)-0.1*(max(y_partial)-min(y_partial)), max(y_partial)+0.1*(max(y_partial)-min(y_partial)))
         ax.set_xlabel(X.columns[f_id])
     axs.append(ax)
     fig.subplots_adjust(bottom=0.15, top=0.7, left=0.1, right=0.95, wspace=0.4,
                         hspace=0.3)
     return fig, axs
+
+#fig, axs = plot_partial_dependence(clf, X_train, range(10), figsize=(15, 10))
