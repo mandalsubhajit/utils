9b32ecc871a850d9f26d0fa8e6e88b0a087a78e7
mandalsubhajit <mandalsubhajit@gmail.com>
2023-05-31 21:19:59 +0530

    Create bert_fine_tuning_for_binary_classification.py

---
+++ bert_fine_tuning_for_binary_classification.py
@@ -0,0 +1,92 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+# from transformers import DataCollatorWithPadding
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+import evaluate
+import numpy as np
+import torch
+
+print(torch.cuda.current_device())
+
+imdb = load_dataset("imdb")
+
+'''
+In[3]: imdb["test"][0]
+Out[3]: 
+{'text': 'I love sci-fi and am willing to put up with a lot. Sci-fi movies/TV are usually underfunded, under-appreciated and misunderstood. I tried to like this, I really did, but it is to good TV sci-fi as Babylon 5 is to Star Trek (the original). Silly prosthetics, cheap cardboard sets, stilted dialogues, CG that doesn\'t match the background, and painfully one-dimensional characters cannot be overcome with a \'sci-fi\' setting. (I\'m sure there are those of you out there who think Babylon 5 is good sci-fi TV. It\'s not. It\'s clichéd and uninspiring.) While US viewers might like emotion and character development, sci-fi is a genre that does not take itself seriously (cf. Star Trek). It may treat important issues, yet not as a serious philosophy. It\'s really difficult to care about the characters here as they are not simply foolish, just missing a spark of life. Their actions and reactions are wooden and predictable, often painful to watch. The makers of Earth KNOW it\'s rubbish as they have to always say "Gene Roddenberry\'s Earth..." otherwise people would not continue watching. Roddenberry\'s ashes must be turning in their orbit as this dull, cheap, poorly edited (watching it without advert breaks really brings this home) trudging Trabant of a show lumbers into space. Spoiler. So, kill off a main character. And then bring him back as another actor. Jeeez! Dallas all over again.',
+ 'label': 0}
+'''
+
+
+
+
+tokenizer = AutoTokenizer.from_pretrained("D:\work\distilbert-base-uncased", model_max_length=512)
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
+
+
+
+
+# data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+
+
+
+accuracy = evaluate.load("accuracy")
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+
+
+id2label = {0: "NEGATIVE", 1: "POSITIVE"}
+label2id = {"NEGATIVE": 0, "POSITIVE": 1}
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    "D:\work\distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
+)
+
+training_args = TrainingArguments(
+    output_dir="D:\work\distilbert-base-uncased-imdb",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=False,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
+)
+
+print(training_args.device)
+trainer.train()
+
+
+
+# MODEL INFERENCE
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("D:\work\distilbert-base-uncased")
+model = AutoModelForSequenceClassification.from_pretrained(
+    "D:\work\distilbert-base-uncased"
+)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
+classifier(text)
