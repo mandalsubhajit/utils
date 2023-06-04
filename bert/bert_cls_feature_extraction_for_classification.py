3cb8ca41f5e866427de347130998b203a1726790
mandalsubhajit <mandalsubhajit@gmail.com>
2023-06-04 15:26:14 +0530

    Update bert_cls_feature_extraction_for_classification.py

--- bert/bert_cls_feature_extraction_for_classification.py
+++ bert/bert_cls_feature_extraction_for_classification.py
@@ -3,7 +3,9 @@ from transformers import AutoTokenizer, AutoModel
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+from typing import List
 
+# 1. LOAD DATA AND TRAIN TEST SPLIT
 datapath = './bbc-text.csv'
 df = pd.read_csv(datapath)
 df.head()
@@ -24,27 +26,28 @@ df_train, df_val = np.split(df.sample(frac=1, random_state=42),
 print(len(df_train),len(df_val))
 
 
+# 2. LOAD BERT MODEL AND TOKENIZER
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 tokenizer = AutoTokenizer.from_pretrained("D:\work\distilbert-base-uncased", model_max_length=512)
 model = AutoModel.from_pretrained("D:\work\distilbert-base-uncased").to(device)
 
 
-
-train_text = df_train["text"].values.tolist()
-val_text = df_val["text"].values.tolist()
-
-def get_cls_embedding(batch, model, tokenizer):
+# 3. EXTRACT BERT [CLS] FEATURE FROM TEXT
+def get_cls_embedding(batch: List[str], model, tokenizer):
     tokenized_batch = tokenizer(batch, padding = True, truncation = True, return_tensors="pt")
     tokenized_batch = {k:torch.tensor(v).to(device) for k,v in tokenized_batch.items()}
     with torch.no_grad():
       hidden_batch = model(**tokenized_batch) #dim : [batch_size(nr_sentences), tokens, emb_dim]
+    # free gpu memory by moving tensors to cpu
     cls_batch = hidden_batch.last_hidden_state[:,0,:].cpu().numpy()
     
     return cls_batch
 
+train_text = df_train["text"].values.tolist()
+val_text = df_val["text"].values.tolist()
 
-
+# process in batches to avoid gpu memory overflow
 batch_size = 10
 
 x_train = np.vstack([get_cls_embedding(train_text[i:i+batch_size], model, tokenizer) for i in tqdm(range(0, len(train_text), batch_size))])
@@ -56,7 +59,8 @@ y_val = df_val["category"]
 print(x_train.shape, y_train.shape, x_val.shape, y_val.shape)
 
 
-# RANDOM FOREST
+# 4. CLASSIFICATION (TRAINING AND TESTING)
+# random forest
 from sklearn.ensemble import RandomForestClassifier
 
 rf = RandomForestClassifier()
@@ -64,23 +68,23 @@ rf.fit(x_train,y_train)
 rf.score(x_val,y_val) 
 
 
-# NEURAL NETWORK
+# neural network
 import tensorflow as tf
-from keras.models import Sequential, Model
-from keras.layers import Dense, Conv1D, AveragePooling1D, Flatten, Input, Lambda, concatenate, Dropout, Reshape
-import keras.backend as K
+from keras.models import Sequential
+from keras.layers import Dense, Input, Dropout
 
 num_features = x_train.shape[1]
 num_classes = 5
 
-model = Sequential()
-model.add(Input(shape=(num_features,)))
-model.add(Dropout(0.5))
-model.add(Dense(num_features//2, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
+network = Sequential()
+network.add(Input(shape=(num_features,)))
+network.add(Dropout(0.5))
+network.add(Dense(num_features//2, activation='relu'))
+network.add(Dropout(0.5))
+network.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
+network.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(network.summary())
 
 label2id = {'business':0,
           'entertainment':1,
@@ -90,4 +94,6 @@ label2id = {'business':0,
           }
 y_tf_train = tf.keras.utils.to_categorical(y_train.map(label2id), num_classes=num_classes)
 y_tf_val = tf.keras.utils.to_categorical(y_val.map(label2id), num_classes=num_classes)
-model.fit(x_train, y_tf_train, validation_data=(x_val, y_tf_val), verbose=1, epochs=5)
+network.fit(x_train, y_tf_train, validation_data=(x_val, y_tf_val), verbose=1, epochs=5)
+
+network.predict(get_cls_embedding('India won the world cup', model, tokenizer))
