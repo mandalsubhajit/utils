55ca166a89e0fee81a7fead1f05ec6943e55e932
mandalsubhajit <mandalsubhajit@gmail.com>
2023-06-10 08:51:54 +0530

    Create summarization_rewards_model.py

---
+++ fine_tuning/summarization/summarization_rewards_model.py
@@ -0,0 +1,147 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Union
+
+import evaluate
+import numpy as np
+import torch.nn as nn
+from datasets import load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    PreTrainedTokenizerBase,
+    Trainer,
+    TrainingArguments,
+)
+from transformers.utils import PaddingStrategy
+
+
+# Load the human comparisons dataset for tuning the reward model.
+ds = load_dataset("openai/summarize_from_feedback", name="comparisons")
+
+# Define the training args. Needs to be done before the model is loaded if you are using deepspeed.
+training_args = TrainingArguments(
+    output_dir="D:\work\distilbert-base-uncased-rm",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=5,
+    weight_decay=0.001,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    save_total_limit=2,
+    gradient_accumulation_steps=4,
+    deepspeed=None,
+    remove_unused_columns=False,
+    label_names=[],
+)
+
+# Load the value-head model and tokenizer.
+tokenizer = AutoTokenizer.from_pretrained('D:\work\distilbert-base-uncased')
+model = AutoModelForSequenceClassification.from_pretrained('D:\work\distilbert-base-uncased', num_labels=1)
+
+
+# Turn the dataset into pairs of post + summaries, where text_j is the preferred post + summary and text_k is the other.
+def preprocess_function(examples):
+    context, better, worse = [], [], []
+    for info, summaries, choice in zip(examples["info"], examples["summaries"], examples["choice"]):
+        if len(summaries) != 2 or choice not in (0, 1):
+            raise ValueError(
+                f"There should be two summaries with a choice that's either 0 or 1. Received {len(summaries)} summaries and choice={choice}."
+            )
+        original_text_field = "post" if info["post"] is not None else "article"
+        context.append(info[original_text_field])
+        better.append(summaries[choice]["text"])
+        worse.append(summaries[0 if choice == 1 else 1]["text"])
+    
+    tokenized_j = tokenizer(better, context,
+                            max_length=384,
+                            truncation="only_second")
+    tokenized_k = tokenizer(worse, context,
+                            max_length=384,
+                            truncation="only_second")
+
+    return {
+        "input_ids_j": tokenized_j["input_ids"],
+        "attention_mask_j": tokenized_j["attention_mask"],
+        "input_ids_k": tokenized_k["input_ids"],
+        "attention_mask_k": tokenized_k["attention_mask"],
+    }
+
+
+original_columns = ds["train"].column_names
+tokenized_ds = ds.map(preprocess_function, batched=True, remove_columns=original_columns)
+
+# We need to define a special data collator that batches the data in our j vs k format.
+@dataclass
+class RewardDataCollatorWithPadding:
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    return_tensors: str = "pt"
+
+    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+        features_j = []
+        features_k = []
+        for feature in features:
+            features_j.append({"input_ids": feature["input_ids_j"], "attention_mask": feature["attention_mask_j"]})
+            features_k.append({"input_ids": feature["input_ids_k"], "attention_mask": feature["attention_mask_k"]})
+        batch_j = self.tokenizer.pad(
+            features_j,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=self.return_tensors,
+        )
+        batch_k = self.tokenizer.pad(
+            features_k,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=self.return_tensors,
+        )
+        batch = {
+            "input_ids_j": batch_j["input_ids"],
+            "attention_mask_j": batch_j["attention_mask"],
+            "input_ids_k": batch_k["input_ids"],
+            "attention_mask_k": batch_k["attention_mask"],
+            "return_loss": True,
+        }
+        return batch
+
+
+# Define the metric that we'll use for validation.
+accuracy = evaluate.load("accuracy")
+
+
+def compute_metrics(eval_pred):
+    predictions, _ = eval_pred
+    # Here, predictions is rewards_j and rewards_k.
+    # We want to see how much of the time rewards_j > rewards_k.
+    predictions = np.argmax(predictions, axis=0)
+    labels = np.zeros(predictions.shape)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+class RewardTrainer(Trainer):
+    # Define how to compute the reward loss.
+    def compute_loss(self, model, inputs, return_outputs=False):
+        rewards_j = model(input_ids=inputs["input_ids_j"], attention_mask=inputs["attention_mask_j"])[0]
+        rewards_k = model(input_ids=inputs["input_ids_k"], attention_mask=inputs["attention_mask_k"])[0]
+        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+        if return_outputs:
+            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
+        return loss
+
+
+# Train the model, woohoo.
+trainer = RewardTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_ds["train"],
+    eval_dataset=tokenized_ds["validation"],
+    compute_metrics=compute_metrics,
+    data_collator=RewardDataCollatorWithPadding(tokenizer=tokenizer),
+)
+
+trainer.train(resume_from_checkpoint=False)
