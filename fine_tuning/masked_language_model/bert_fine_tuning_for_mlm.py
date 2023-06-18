636527b75163a588a3585862e7f7a0ad8c7e60f9
mandalsubhajit <mandalsubhajit@gmail.com>
2023-06-18 07:49:35 +0530

    Create bert_fine_tuning_for_mlm.py

---
+++ fine_tuning/masked_language_model/bert_fine_tuning_for_mlm.py
@@ -0,0 +1,112 @@
+from datasets import load_dataset
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+from transformers import DataCollatorForLanguageModeling
+from transformers import TrainingArguments, Trainer
+
+imdb_dataset = load_dataset("imdb")
+imdb_dataset
+
+
+model_checkpoint = "D:\work\distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForMaskedLM.from_pretrained(model_checkpoint)
+
+# tokenization
+def tokenize_function(examples):
+    result = tokenizer(examples["text"])
+    if tokenizer.is_fast:
+        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
+    return result
+
+
+# Use batched=True to activate fast multithreading!
+tokenized_datasets = imdb_dataset.map(
+    tokenize_function, batched=True, remove_columns=["text", "label"]
+)
+tokenized_datasets
+
+
+# split corpus into chunks of text (chunk_size based on gpu memory)
+chunk_size = 128
+
+def group_texts(examples):
+    # Concatenate all texts
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    # Compute length of concatenated texts
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the last chunk if it's smaller than chunk_size
+    total_length = (total_length // chunk_size) * chunk_size
+    # Split by chunks of max_len
+    result = {
+        k: [t[i : i + chunk_size] for i in range(0, total_length, chunk_size)]
+        for k, t in concatenated_examples.items()
+    }
+    # Create a new labels column
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+lm_datasets = tokenized_datasets.map(group_texts, batched=True)
+lm_datasets
+
+# random token masking for training
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+
+
+# downsampling data for fast and light training
+train_size = 10_000
+test_size = int(0.1 * train_size)
+
+downsampled_dataset = lm_datasets["train"].train_test_split(
+    train_size=train_size, test_size=test_size, seed=42
+)
+downsampled_dataset
+
+
+# training
+batch_size = 64
+# Show the training loss with every epoch
+logging_steps = len(downsampled_dataset["train"]) // batch_size
+
+training_args = TrainingArguments(
+    output_dir=f"D:\\work\\distilbert-base-uncased-finetuned-imdb",
+    overwrite_output_dir=True,
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    weight_decay=0.01,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    push_to_hub=False,
+    fp16=True,
+    logging_steps=logging_steps,
+)
+
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=downsampled_dataset["train"],
+    eval_dataset=downsampled_dataset["test"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
+
+
+
+
+# INFERENCE
+from transformers import AutoModelForMaskedLM, AutoTokenizer, pipeline
+
+model_checkpoint = "D:\work\distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForMaskedLM.from_pretrained(model_checkpoint)
+
+mask_filler = pipeline(
+    "fill-mask", model=model, tokenizer=tokenizer
+)
+
+preds = mask_filler(text)
+
+for pred in preds:
+    print(f">>> {pred['sequence']}")
